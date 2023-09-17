5d49967fd91e85b7856be6d1871650ed2bddea52
saimonyaky <116937644+saimonyaky@users.noreply.github.com>
2023-09-17 17:32:50 +0700

    sapxep

---
+++ Webshop/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admins', 'AdminController@index')->name('admins');
+Route::get('/category', 'CategoryController@index')->name('category');
